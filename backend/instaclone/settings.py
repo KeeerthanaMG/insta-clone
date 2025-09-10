73ab8ab93adf8824320d89ba9166c9ffa9932618
KeeerthanaMG <keerthanamg19@gmail.com>
2025-09-10 07:44:57 +0530

    postgres transfer initiated

--- backend/instaclone/settings.py
+++ backend/instaclone/settings.py
@@ -56,7 +56,11 @@ ASGI_APPLICATION = "instaclone.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": config("DATABASE_ENGINE", default="django.db.backends.sqlite3"),
-        "NAME": BASE_DIR / config("DATABASE_NAME", default="db.sqlite3"),
+        "NAME": config("DATABASE_NAME", default="db.sqlite3"),
+        "USER": config("DATABASE_USER", default=""),
+        "PASSWORD": config("DATABASE_PASSWORD", default=""),
+        "HOST": config("DATABASE_HOST", default="localhost"),
+        "PORT": config("DATABASE_PORT", default="5432"),
     }
 }
 
